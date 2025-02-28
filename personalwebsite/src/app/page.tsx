02996509fc5b47a438cf53d900b8242a4168340a
Vegito2367 <tejaaditya.g@gmail.com>
2025-02-28 13:16:45 -0600

    Initial commit

---
+++ personalwebsite/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="flex flex-row justify-center">
+        <p>
+          Hello World
+        </p>
+        
+      </section>
+    </>
+  );
+}
