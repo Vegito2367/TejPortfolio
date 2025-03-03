2880219e148447c5f82da1c9e2f624c1ad5802bc
Vegito2367 <tejaaditya.g@gmail.com>
2025-03-03 16:04:58 -0600

    Saving work

--- personalwebsite/src/app/page.tsx
+++ personalwebsite/src/app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
     </AnimatePresence>
     {showMainContent && (
       <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1}} className="flex flex-row justify-center items-center roboto-mono mx-28 border-2 border-blue-300">
-        <section className="flex flex-row justify-center items-center w-4/6 h-screen">
-          <div className="w-1/2">
-          <p>Hello Everyone!</p>
-          <p>I am Tej Gumaste, a junior studying computer science interested in impactful coding</p>
+        <section className="flex flex-row justify-center items-center p-4 h-screen">
+          <div className="w-4/6">
+          <p className="text-4xl">Hello Everyone!</p>
+          <p className="text-3xl">I am Tej Gumaste, a junior studying computer science interested in impactful coding</p>
           </div>
           <img src="ProfilePic.jpeg" alt="Profile Picture" className="w-1/3 rounded-3xl" />
         </section>
