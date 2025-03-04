c64b0a99c8f17a353539c320fab2023603a26e8a
Vegito2367 <tejaaditya.g@gmail.com>
2025-03-04 12:41:24 -0600

    Wrapping up, need to study for databases

--- personalwebsite/src/app/page.tsx
+++ personalwebsite/src/app/page.tsx
@@ -3,7 +3,8 @@ import { AnimatePresence, motion } from "motion/react";
 import ThreeScene from "@/customComponents/threeScene";
 import Loading from "@/customComponents/loading";
 import { useState } from "react";
-import { image } from "motion/react-client";
+
+
 export default function Home() {
   const [showMainContent, setShowMainContent] = useState(false);
   function handleLoadingEnd() {
@@ -15,6 +16,8 @@ export default function Home() {
       {!showMainContent &&(<Loading callback={handleLoadingEnd}/>)}
     </AnimatePresence>
     {showMainContent && (
+      <>
+
       <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1}} className="flex flex-row justify-center items-center roboto-mono mx-28 border-2 border-blue-300">
         <section className="flex flex-row justify-center items-center p-4 h-screen">
           <div className="w-4/6">
@@ -24,6 +27,8 @@ export default function Home() {
           <img src="ProfilePic.jpeg" alt="Profile Picture" className="w-1/3 rounded-3xl" />
         </section>
       </motion.div>
+      </>
+      
     )}
       
     </div>
