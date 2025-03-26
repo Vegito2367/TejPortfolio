ee087fef3d8fa41d21912b1808774a181a059ac7
Vegito2367 <tejaaditya.g@gmail.com>
2025-03-25 20:06:04 -0500

    Major changes again

--- personalwebsite/src/app/page.tsx
+++ personalwebsite/src/app/page.tsx
@@ -2,80 +2,79 @@
 import { AnimatePresence, motion } from "motion/react";
 import ThreeScene from "@/customComponents/threeScene";
 import Loading from "@/customComponents/loading";
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Experiences from "@/customComponents/experience";
 import Projects from "@/customComponents/projects";
-import { pre } from "framer-motion/client";
+import HeroSection from "@/customComponents/heroSection";
 
 
 export default function Home() {
   const [showMainContent, setShowMainContent] = useState(false);
-  const [followPos, setFollowPos] = useState({x: 0, y: 0});
+  const [followPos, setFollowPos] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    function handleMouseMove(e:MouseEvent) {
-      setFollowPos({x: e.pageX, y: e.pageY});
+    function handleMouseMove(e: MouseEvent) {
+      setFollowPos({ x: e.pageX, y: e.pageY });
     }
-    
-    // Add event listener to the window
+
     window.addEventListener('mousemove', handleMouseMove);
-    
-    // Clean up the event listener when component unmounts
+
+
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     }
-  }, []); // E
+  }, []);
   function handleLoadingEnd() {
     setShowMainContent(true);
   }
 
-  
+
   return (
-    
-    <div className="flex flex-col justify-center items-center font-mono">
-    <div 
+
+    <div style={{cursor:"none"}} className="flex flex-col justify-center items-center font-mono">
+      <div
         style={{
-          top:`${followPos.y}px`,
-          left:`${followPos.x}px`, 
-          transform: 'translate(-50%, -50%)', 
-          transition:"all", 
-          transitionDuration:"0.01s",
+          top: `${followPos.y}px`,
+          left: `${followPos.x}px`,
+          transform: 'translate(-50%, -50%)',
+          transition: "all",
+          transitionDuration: "0.01s",
           pointerEvents: "none",
           zIndex: 50
-        }} 
-        className="rounded-full absolute h-3 w-3 bg-orange-200" 
+        }}
+        className="rounded-full absolute h-3 w-3 bg-orange-200"
       />
-      <div 
+      <div
         style={{
-          top:`${followPos.y}px`,
-          left:`${followPos.x}px`, 
-          transform: 'translate(-50%, -50%)', 
-          transition:"all", 
-          transitionDuration:"0.013s",
+          top: `${followPos.y}px`,
+          left: `${followPos.x}px`,
+          transform: 'translate(-50%, -50%)',
+          transition: "all",
+          transitionDuration: "0.013s",
           pointerEvents: "none",
-          zIndex: 50
-        }} 
-        className="rounded-full absolute h-3 w-3 bg-orange-400" 
+          zIndex: 49
+        }}
+        className="rounded-full absolute h-4 w-4 bg-orange-400"
       />
-      <div 
+      <div
         style={{
-          top:`${followPos.y}px`,
-          left:`${followPos.x}px`, 
-          transform: 'translate(-50%, -50%)', 
-          transition:"all", 
-          transitionDuration:"0.016s",
+          top: `${followPos.y}px`,
+          left: `${followPos.x}px`,
+          transform: 'translate(-50%, -50%)',
+          transition: "all",
+          transitionDuration: "0.016s",
           pointerEvents: "none",
-          zIndex: 50
-        }} 
-        className="rounded-full absolute h-3 w-3 bg-orange-600" 
-      />  
-     <AnimatePresence>
-      {!showMainContent &&(<Loading callback={handleLoadingEnd}/>)}
-    </AnimatePresence>
-    {showMainContent && (
-      <div>
-      
-      <motion.div 
+          zIndex: 48
+        }}
+        className="rounded-full absolute h-5 w-5 bg-orange-600"
+      />
+      <AnimatePresence>
+        {!showMainContent && (<Loading callback={handleLoadingEnd} />)}
+      </AnimatePresence>
+      {showMainContent && (
+        <div>
+
+          {/* <motion.div 
         initial={{ opacity: 0, scale: 0.5 }} 
         animate={{ opacity: 1, scale:1 }} 
         transition={{ duration: 0.7 }} 
@@ -95,25 +94,25 @@ export default function Home() {
             className="w-2/5 max-w-xs md:max-w-md rounded-3xl shadow-lg border-4 border-gray-700 mt-6 md:mt-0"
           />
         </section>
-      </motion.div>
-    
+      </motion.div> */}
+      <HeroSection />
 
-      <motion.div 
-        initial={{ x: 1000, opacity: 0 }} 
-        animate={{ x: 0, opacity: 1 }} 
-        transition={{ duration: 1.75 }} 
-        className="flex flex-col items-start w-full p-4"
-      >
-        <Experiences />
-      </motion.div>
-    
+          <motion.div
+            initial={{ x: 1000, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.75 }}
+            className="flex flex-col items-start w-full p-4"
+          >
+            <Experiences />
+          </motion.div>
 
-      <Projects />
-    </div>
-    
-      
-    )}
-      
+
+          <Projects />
+        </div>
+
+
+      )}
+
     </div>
   );
 }
