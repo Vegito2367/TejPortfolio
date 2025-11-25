b51ddca065fa8613dd870fbc5b8b16ba03449a77
Vegito2367 <tejaaditya.g@gmail.com>
2025-11-24 21:25:51 -0600

    started minimal website

--- personalwebsite/src/app/layout.tsx
+++ personalwebsite/src/app/layout.tsx
@@ -1,26 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "700", "800"], // Select the weights you need
+  variable: "--font-jetbrains-mono", // Define a CSS variable name
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "Tej Gumaste",
   description: "Tej's Portfolio",
-  icons:{
-    icon:"/icon.png",
+  icons: {
+    icon: "/icon.png",
     apple: "/icon.png",
-    
-  }
+  },
 };
 
 export default function RootLayout({
@@ -29,10 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
-      </body>
+    <html lang="en" className={jetbrainsMono.variable}>
+      <body className="font-mono antialiased">{children}</body>
     </html>
   );
 }
--- personalwebsite/src/app/page.tsx
+++ personalwebsite/src/app/page.tsx
@@ -1,10 +1,7 @@
-"use client"
+"use client";
+import { p } from "framer-motion/client";
 import { AnimatePresence, motion } from "motion/react";
 import React, { useState, useEffect, useRef } from "react";
-import Experiences from "@/customComponents/experience";
-import Projects from "@/customComponents/projects";
-import HeroSection from "@/customComponents/heroSection";
-
 
 export default function Home() {
   const cursorRef1 = useRef<HTMLDivElement>(null);
@@ -23,61 +20,91 @@ export default function Home() {
       }
     }
 
-    window.addEventListener('mousemove', handleMouseMove);
-
+    window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    }
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
   }, []);
 
-
   return (
-
-    <div style={{ cursor: "none" }} className="flex flex-col justify-center items-center font-mono">
+    <div
+      style={{ cursor: "none" }}
+      className="flex flex-col justify-center items-center font-mono"
+    >
       <div
         ref={cursorRef1}
         style={{
-          transform: 'translate(-50%, -50%)',
+          transform: "translate(-50%, -50%)",
           pointerEvents: "none",
-          zIndex: 50
+          zIndex: 50,
         }}
         className="rounded-full absolute h-3 w-3 bg-orange-200"
       />
       <div
         ref={cursorRef2}
         style={{
-          transform: 'translate(-50%, -50%)',
+          transform: "translate(-50%, -50%)",
           pointerEvents: "none",
-          zIndex: 49
+          zIndex: 49,
         }}
         className="rounded-full absolute h-4 w-4 bg-orange-400"
       />
       <div
         ref={cursorRef3}
         style={{
-          transform: 'translate(-50%, -50%)',
+          transform: "translate(-50%, -50%)",
           pointerEvents: "none",
-          zIndex: 48
+          zIndex: 48,
         }}
         className="rounded-full absolute h-5 w-5 bg-orange-600"
       />
-        <div className="bg-gradient-to-br from-gray-900 to-black">
+      <div className="bg-gradient-to-br from-gray-800 to-black h-full w-full">
+        <section className="px-20 py-20 justify-center items-center">
+          <p className="text-5xl text-center my-2"> Tej Gumaste</p>
+          <p className="text-gray-500 text-center">
+            Computer Science & Mathematics @ University of Kansas
+          </p>
+          <p className="text-center my-2">
+            Also known as @Vegito2367, @seksierthanyou and @TejG17
+          </p>
+          <p className="my-2">
+            Hello I am Tej, a 21 year old computer science student with a
+            fondness for programming. I have worked with lots of technologies
+            and platforms over the years and I plan to keep trying new things
+            throughout my career. I believe that any programming language is
+            only as important as the problem it solves, so I don't really have a
+            favorite language, although some languages are easier to work with
+            than others.
+          </p>
 
-          <HeroSection />
-
-          <motion.div
-            initial={{ x: 1000, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1.75 }}
-            className="flex flex-col items-start w-full p-4"
-          >
-            <Experiences />
-          </motion.div>
-
-
-          <Projects />
-        </div>
+          <div>
+            <p className="text-3xl underline">Experience</p>
+            <br />
+            <p>Gemini</p>
+            <p>
+              Interned there summer of 2025 and my project was to build a wire
+              payment rail system. I implemented the wire return feature which
+              was pretty fun and the project is expected to earn the company
+              $4M/year
+            </p>
+            <br />
+            <p>Research Engineer @ Blakemore Lab KU</p>
+            <p>
+              Worked with Professor James Blakemore and Fynn Cooper to build
+              <a href="https://github.com/Vegito2367/PyCIFTer">PyCIFTer</a>.
+              PyCIFTer is a program that parses crystallographic information
+              files (CIFs). CIFs are the primary medium of data representation
+              for inorganic crystal structures and PyCIFTer is a origin-agnostic
+              way to parse CIFs and mass analyze crystal structures.
+              <span className="font-bold">
+                PyCIFTer is currently undergoing the peer review process to be
+                published in Molecules.
+              </span>
+            </p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
